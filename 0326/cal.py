0ecb88120bf38493059160e1c7e28b3ebc922940
ssandylin <37054703+ssandylin@users.noreply.github.com>
2018-03-27 15:47:21 +0800

    calculator

---
+++ 0326/cal.py
@@ -0,0 +1,12 @@
+﻿def number():
+        y = int(input("請輸入數字1:"));x = input("請輸入符號");z = int(input("請輸入數字2:"))
+        if x == "+":
+            return("結果:",y+z)
+        elif x == "-":
+            return("結果:",y-z)
+        elif x == "*":
+            return("結果:",y*z)
+        elif x == "%":
+            return("結果:",y%z)
+        else:
+            return("輸入錯誤")
---
+++ 0326/calculator.py
@@ -0,0 +1,6 @@
+﻿import cal
+while True:
+    print(cal.number())
+    x = input("若要離開請按Q")
+    if x == "Q":
+        break
