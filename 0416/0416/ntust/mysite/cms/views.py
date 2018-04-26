516ee9d6203759e12b9158ea53cc01dd2b0d9c76
ssandylin <37054703+ssandylin@users.noreply.github.com>
2018-04-26 22:56:21 +0800

    hw

--- 0416/0416/ntust/mysite/cms/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from .models import person
-
-def index(request):
-	Person=person.objects.all()
-	return render_to_response('cms/menu.html',locals())
-
-# Create your views here.
